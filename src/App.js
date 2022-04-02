8f558450f304b98150556a47bb23fc6741ffcb2f
Hawk <tsasha1208@rambler.ru>
2022-04-02 12:12:17 +0300

    Zaebali

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+  const [merchants, setMerchants] = useState(false);
+
+  useEffect(() => {
+    getMerchant();
+  }, []);
+
+  function getMerchant() {
+    fetch('http://localhost:3001')
+        .then(response => {
+          return response.text();
+        })
+        .then(data => {
+          setMerchants(data);
+        });
+  }
+
+  function createMerchant() {
+    let name = prompt('Enter merchant name');
+    let email = prompt('Enter merchant email');
+
+    fetch('http://localhost:3001/merchants', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({name, email}),
+    })
+        .then(response => {
+          return response.text();
+        })
+        .then(data => {
+          alert(data);
+          getMerchant();
+        });
+  }
+
+  function deleteMerchant() {
+    let id = prompt('Enter merchant id');
+
+    fetch(`http://localhost:3001/merchants/${id}`, {
+      method: 'DELETE',
+    })
+        .then(response => {
+          return response.text();
+        })
+        .then(data => {
+          alert(data);
+          getMerchant();
+        });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App-header">
+        {merchants ? merchants : 'There is no merchant data available'}
+        <br /><br />
+        <div>
+          <button className="App-button" onClick={createMerchant}>Add</button>
+          <button className="App-button" onClick={deleteMerchant}>Delete</button>
+        </div>
+      </div>
   );
 }
 
